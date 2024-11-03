c7c56ce902d23ef5fe404d1764da920cef79f14b
virtual-float <krystianturek5@outlook.com>
2024-11-03 15:37:47 +0100

    Added components base class

--- main.py
+++ main.py
@@ -1,7 +1,14 @@
 import asyncio, pygame
 
+pygame.init()
+
+
 async def main():
-    pass
+    
+    print(pygame.get_init())
+
+    pygame.quit()
+
 
 if __name__ == "__main__":
     asyncio.run(main())
